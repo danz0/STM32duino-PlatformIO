3b68b522653ebc9f7284dd396bc8e472728f3669
ubis <ubitas52@gmail.com>
2016-06-16 13:15:19 +0300

    Migration to PlatformIO 3.0

---
+++ builder/frameworks/arduino.py
@@ -0,0 +1,108 @@
+# Copyright 2014-present Ivan Kravets <me@ikravets.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+STM32duino
+
+STM32 Arduino Wiring-based Framework allows writing cross-platform software to
+control devices attached to a wide range of Arduino boards to create all
+kinds of creative coding, interactive objects, spaces or physical experiences.
+
+http://www.stm32duino.com
+"""
+
+from os import walk
+from os.path import isdir, isfile, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.DevPlatform()
+
+if "stm32f103" in env.BoardConfig().get("build.mcu", ""):
+    FRAMEWORK_DIR = join(platform.get_package_dir("framework-stm32duino"), "STM32F1")
+
+FRAMEWORK_VERSION = platform.get_package_version("framework-stm32duino")
+assert isdir(FRAMEWORK_DIR)
+
+ARDUINO_VERSION = int(FRAMEWORK_VERSION.replace(".", "").strip())
+
+# USB flags
+ARDUINO_USBDEFINES = ["ARDUINO=%s" % FRAMEWORK_VERSION.split(".")[1]]
+
+env.Append(
+    CPPDEFINES=ARDUINO_USBDEFINES,
+
+    CPPPATH=[
+        join("$BUILD_DIR", "FrameworkArduino"),
+        join(FRAMEWORK_DIR, "system", "libmaple"),
+        join(FRAMEWORK_DIR, "system", "libmaple", "include"),
+        join(FRAMEWORK_DIR, "system", "libmaple", "usb", "stm32f1"),
+        join(FRAMEWORK_DIR, "system", "libmaple", "usb", "usb_lib")
+    ],
+
+    LIBPATH=[
+        join(FRAMEWORK_DIR, "variants", env.BoardConfig().get("build.variant"), "ld")
+    ]
+)
+
+env.VariantDirWrap(
+    join("$BUILD_DIR", "FrameworkArduinoInc"),
+    join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"))
+)
+
+
+#
+# Lookup for specific core's libraries
+#
+
+BOARD_CORELIBDIRNAME = env.BoardConfig().get("build.core", "")
+env.Append(
+    LIBSOURCE_DIRS=[
+        join(FRAMEWORK_DIR, "libraries", "__cores__", BOARD_CORELIBDIRNAME),
+        join(FRAMEWORK_DIR, "libraries")
+    ]
+)
+
+#
+# Target: Build Core Library
+#
+
+libs = []
+
+if "build.variant" in env.BoardConfig():
+    env.Append(
+        CPPPATH=[
+            join("$BUILD_DIR", "FrameworkArduinoVariant")
+        ]
+    )
+    libs.append(env.BuildLibrary(
+        join("$BUILD_DIR", "FrameworkArduinoVariant"),
+        join(FRAMEWORK_DIR, "variants", env.BoardConfig().get("build.variant"))
+    ))
+
+envsafe = env.Clone()
+
+libs.append(envsafe.BuildLibrary(
+    join("$BUILD_DIR", "FrameworkArduino"),
+    join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"))
+))
+
+env.Append(
+    LIBPATH=[
+        join(FRAMEWORK_DIR, "variants", env.BoardConfig().get("build.variant"))
+    ]
+)
+
+env.Prepend(LIBS=libs)
---
+++ builder/main.py
@@ -0,0 +1,193 @@
+# Copyright 2014-present Ivan Kravets <me@ikravets.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+    Builder for STM32 series of microcontrollers
+"""
+
+from os.path import basename, join
+
+from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild, Builder, Default,
+                          DefaultEnvironment)
+
+from platformio.util import get_serialports
+
+
+def BeforeUpload(target, source, env):  # pylint: disable=W0613,W0621
+    env.AutodetectUploadPort()
+
+    upload_options = {}
+    if "BOARD" in env:
+        upload_options = env.BoardConfig().get("upload", {})
+
+    if not upload_options.get("disable_flushing", False):
+        env.FlushSerialBuffer("$UPLOAD_PORT")
+
+    before_ports = get_serialports()
+
+    if upload_options.get("use_1200bps_touch", False):
+        env.TouchSerialPort("$UPLOAD_PORT", 1200)
+
+    if upload_options.get("wait_for_upload_port", False):
+        env.Replace(UPLOAD_PORT=env.WaitForNewSerialPort(before_ports))
+
+    # use only port name for BOSSA
+    if "/" in env.subst("$UPLOAD_PORT"):
+        env.Replace(UPLOAD_PORT=basename(env.subst("$UPLOAD_PORT")))
+
+
+env = DefaultEnvironment()
+
+env.Replace(
+    AR="arm-none-eabi-ar",
+    AS="arm-none-eabi-as",
+    CC="arm-none-eabi-gcc",
+    CXX="arm-none-eabi-g++",
+    OBJCOPY="arm-none-eabi-objcopy",
+    RANLIB="arm-none-eabi-ranlib",
+    SIZETOOL="arm-none-eabi-size",
+
+    ARFLAGS=["rcs"],
+
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CFLAGS=[
+        "-std=gnu11"
+    ],
+
+    CCFLAGS=[
+        "-g",   # include debugging info (so errors include line numbers)
+        "-Os",  # optimize for size
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-Wall",
+        "-mthumb",
+        "-nostdlib",
+        "--param", "max-inline-insns-single=500"
+    ],
+
+    CXXFLAGS=[
+        "-fno-rtti",
+        "-fno-exceptions",
+        "-std=gnu++11",
+        "-fno-threadsafe-statics"
+    ],
+
+    CPPDEFINES=[
+        "F_CPU=$BOARD_F_CPU"
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-Wl,--gc-sections,--relax",
+        "-mthumb",
+        "-Wl,--check-sections",
+        "-Wl,--unresolved-symbols=report-all",
+        "-Wl,--warn-common",
+        "-Wl,--warn-section-align",
+        "$BUILD_DIR/FrameworkArduinoVariant/wirish/syscalls.o"
+    ],
+
+    LIBS=["c", "gcc", "m"],
+
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    PROGNAME="firmware",
+    PROGSUFFIX=".elf"
+)
+
+if "BOARD" in env:
+    env.Append(
+        CCFLAGS=[
+            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+        ],
+        LINKFLAGS=[
+            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+        ]
+    )
+
+env.Append(
+    ASFLAGS=env.get("CCFLAGS", [])[:],
+
+    BUILDERS=dict(
+        ElfToBin=Builder(
+            action=" ".join([
+                "$OBJCOPY",
+                "-O",
+                "binary",
+                "$SOURCES",
+                "$TARGET"]),
+            suffix=".bin"
+        ),
+        ElfToHex=Builder(
+            action=" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"]),
+            suffix=".hex"
+        )
+    )
+)
+
+env.Replace(
+    UPLOADER=join(env.DevPlatform().get_package_dir("framework-stm32duino"), "tools", "win", "serial_upload"), 
+    UPLOADERFLAGS=["$UPLOAD_PORT"],
+    UPLOADCMD='$UPLOADER $UPLOAD_PORT {upload.altID} {upload.usbID} $PROJECT_DIR\$SOURCES'
+)
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = env.BuildProgram()
+
+#
+# Target: Build the .bin file
+#
+
+if "uploadlazy" in COMMAND_LINE_TARGETS:
+    target_firm = join("$BUILD_DIR", "firmware.bin")
+else:
+    target_firm = env.ElfToBin(join("$BUILD_DIR", "firmware"), target_elf)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias("size", target_elf, "$SIZEPRINTCMD")
+AlwaysBuild(target_size)
+
+#
+# Target: Upload by default .bin file
+#
+
+upload = env.Alias(["upload", "uploadlazy"], target_firm, [BeforeUpload, "$UPLOADCMD"])
+
+AlwaysBuild(upload)
+
+#
+# Target: Unit Testing
+#
+
+AlwaysBuild(env.Alias("test", [target_firm, target_size]))
+
+#
+# Setup default targets
+#
+
+Default([target_firm, target_size])
